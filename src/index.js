90e36a82379e99b020dd963822956025bbd5b6e1
SiljeP <u2440913@rts-365.dk>
2023-11-02 12:22:31 +0100

    slider works with touch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import "./style.scss"
-import "./scripts/empty"
-import "./images/4c84bb42b56f399c7c2ccee9d422f1cd.png"
-import "./images/HarryStylesWembley170623_(65_of_93)_(52982678051)_(cropped_2).jpg"
+import "./scripts/touch"
 
-const headline = "welcome to the webpage"
-document.querySelector("h1").innerText = headline
